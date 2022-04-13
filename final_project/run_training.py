92e8baaa24f1f12e5cbc5eb42a04b4db78d88339
Khai Yi Chin <khaiyichin@gmail.com>
2022-04-13 15:30:45 -0400

    Added working directory argument.

--- final_project/run_training.py
+++ final_project/run_training.py
@@ -1,14 +1,19 @@
 import util_modules as utm
+import os
 import argparse
 
 # Parse arguments
 parser = argparse.ArgumentParser(description="DS 595-ML Final Project")
 parser.add_argument('config', type=str, help='path to configuration file')
+parser.add_argument('working_dir', type=str, help='working directory to run the training')
 parser.add_argument('--no_save', action="store_true", help='path to configuration file')
 
 args = parser.parse_args()
 
 if __name__ == "__main__":
+
+    os.chdir(args.working_dir) # change to desired working directory
+
     nn_trainer = utm.NNTrainer(args)
 
     # Run trainer
